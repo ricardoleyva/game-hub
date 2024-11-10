830708ed22de7f340633924c3c9c4d18f03918c9
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-11-10 08:48:29 -0800

    Created generic fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,11 @@
 import useGenres from "@/hooks/useGenres";
 
 const GenreList = () => {
-  const { genres } = useGenres();
+  const { data } = useGenres();
   return (
     <ul>
-      {genres.map((genres) => (
-        <li key={genres.id}>{genres.name}</li>
+      {data.map((data) => (
+        <li key={data.id}>{data.name}</li>
       ))}
     </ul>
   );
