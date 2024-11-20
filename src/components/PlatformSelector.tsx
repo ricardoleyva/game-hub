d13d08ab1ed0873568b082203aedbded3d759872
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-11-19 20:17:49 -0500

    Platform selector hook done

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,3 +1,4 @@
+import { Platform } from "@/hooks/useGames";
 import usePlatforms from "@/hooks/usePlatforms";
 import {
   Button,
@@ -8,21 +9,28 @@ import {
 } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const PlatformSelector = () => {
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
   if (error) return null;
   return (
     <MenuRoot>
       <MenuTrigger asChild>
         <Button>
-          {" "}
-          Platforms <BsChevronDown />
+          {selectedPlatform?.name || "Platforms"} <BsChevronDown />
         </Button>
       </MenuTrigger>
       <MenuContent width="150px" position="fixed">
         {data.map((p) => (
-          <MenuItem key={p.id} value={p.slug}>
-            {p.name}
+          <MenuItem
+            onClick={() => onSelectPlatform(p)}
+            key={p.id}
+            value={p.name}
+          >
+            {p.name} ID: {p.id}
           </MenuItem>
         ))}
       </MenuContent>
